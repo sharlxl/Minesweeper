468efd67925a82901d9fa040b58cf1a160deb6d3
sharlxl <sharleneee@gmail.com>
2022-02-27 18:05:15 +0800

    added randomised bombs and css changes to the game board

--- script.js
+++ script.js
@@ -2,12 +2,20 @@
 
 const board = document.querySelector(".board");
 const width = 10;
+const bombs = 20;
 const tiles = [];
 
 function createBoard() {
+  const bombsArray = Array(bombs).fill("bomb");
+  const safeArray = Array(width * width - bombs).fill("safe");
+  const combinedArray = safeArray.concat(bombsArray);
+  const shuffledArray = combinedArray.sort(() => Math.random() - 0.5);
+  console.log(shuffledArray);
+
   for (let i = 0; i < width * width; i++) {
     const tile = document.createElement("div");
     tile.setAttribute("id", i);
+    tile.classList = shuffledArray[i];
     board.appendChild(tile);
     tiles.push(tile);
   }
