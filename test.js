4e2b7e83e2b7753126f4260a0ece0d1ae360ab51
sharlxl <sharleneee@gmail.com>
2022-03-03 10:52:12 +0800

    css adjustments

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 "use strict";
 
 let rows = 10;
-let columns = 10;
+let cols = 10;
+const bombs;
 
-function createBoard() {
+
+function createGrid(rows, cols) {
   for (let i = 0; i < rows; i++) {
-    for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
       let btn = document.createElement("button");
     }
   }
